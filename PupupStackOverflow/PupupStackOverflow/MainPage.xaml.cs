9d3657047a8ca02fc29207e5c62d7224f71640ff
bashobbel <bashobbel@gmail.com>
2021-10-26 21:29:01 +0200

    Add project files.

---
+++ PupupStackOverflow/PupupStackOverflow/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Rg.Plugins.Popup.Animations;
+using Rg.Plugins.Popup.Enums;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace PupupStackOverflow
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var pr = new Popup();
+            var scaleAnimation = new ScaleAnimation
+            {
+                PositionIn = MoveAnimationOptions.Right,
+                PositionOut = MoveAnimationOptions.Left
+            };
+
+            pr.Animation = scaleAnimation;
+            await PopupNavigation.PushAsync(pr);
+        }
+    }
+}
---
+++ PupupStackOverflow/PupupStackOverflow/Popup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rg.Plugins.Popup.Animations;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PupupStackOverflow
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Popup //: PopupPage
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+        
+      
+    }
+}
